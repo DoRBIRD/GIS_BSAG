b15c89ab12caa515ad8bad82f1821f968873676f
mikinho90 <mikinho90@gmx.de>
2018-06-07 23:52:07 +0200

    Pathing now implemented with Inputfields

--- qgisToWeb/qgis2web_Leaflet/js/Graph.js
+++ qgisToWeb/qgis2web_Leaflet/js/Graph.js
@@ -7,6 +7,10 @@ Graph.prototype.add = function(name, edges) {
     this.vertices[name] = edges;
 };
 
+Graph.prototype.getVertices = function() {
+    return this.vertices;
+}
+
 Graph.prototype.findShortestPath = function(source, end) {
 
     var remainingNodes = this.vertices;
@@ -21,32 +25,37 @@ Graph.prototype.findShortestPath = function(source, end) {
 
     distanceOfNodes[source] = 0;
 
-    while (Object.keys(remainingNodes).length !== 0) {
+    if (this.vertices[source] && this.vertices[end]) {
+        while (Object.keys(remainingNodes).length !== 0) {
 
-        var currentNode = getNodeWithMinimumDistance(remainingNodes, distanceOfNodes);
+            var currentNode = getNodeWithMinimumDistance(remainingNodes, distanceOfNodes);
 
-        for (var neighborNode in this.vertices[currentNode]) {
-            var alternateDistance = distanceOfNodes[currentNode]
-                + distanceBetweenNodes(this.vertices, currentNode, neighborNode);
+            for (var neighborNode in this.vertices[currentNode]) {
+                 var alternateDistance = distanceOfNodes[currentNode]
+                        + distanceBetweenNodes(this.vertices, currentNode, neighborNode);
 
-            if (alternateDistance < distanceOfNodes[neighborNode]) {
-                distanceOfNodes[neighborNode] = alternateDistance;
-                previousNodes[neighborNode] = currentNode;
+                 if (alternateDistance < distanceOfNodes[neighborNode]) {
+                    distanceOfNodes[neighborNode] = alternateDistance;
+                    previousNodes[neighborNode] = currentNode;
+                 }
             }
+
+            delete remainingNodes[currentNode];
         }
 
-        delete remainingNodes[currentNode];
+        var path = [];
+        var currentNode = end;
+        while (distanceOfNodes[currentNode] !== 0) {
+            path.push(currentNode);
+            currentNode = previousNodes[currentNode];
+        }
+        path.push(source);
+
+        return path.reverse();
     }
 
-    var path = [];
-    var currentNode = end;
-    while (distanceOfNodes[currentNode] !== 0) {
-        path.push(currentNode);
-        currentNode = previousNodes[currentNode];
-    }
-    path.push(source);
+    return "Keine Strecke möglich!";
 
-    return path.reverse();
 };
 
 function distanceBetweenNodes(vertices, currentNode, targetNode) {
